ad3f5821416346dfe63af6400278d7f644b08b77
Tieda Wei <weitieda@gmail.com>
2021-01-28 12:03:34 -0500

    Refactor

--- Algorithms/AlgorithmsTests/ValidateSubsequenceTest.swift
+++ Algorithms/AlgorithmsTests/ValidateSubsequenceTest.swift
@@ -12,20 +12,26 @@ class ValidateSubsequenceTest: XCTestCase {
     
     func test1() {
         let input = [5, 1, 22, 25, 6, -1, 8, 10]
-        XCTAssertTrue(ValidateSubsequence.solution(input: input, sequence: [1, 6, -1, 10]))
-        XCTAssertTrue(ValidateSubsequence.solution2(input: input, sequence: [1, 6, -1, 10]))
+        let seq = [1, 6, -1, 10]
+        
+        XCTAssertTrue(ValidateSubsequence.solution(input: input, sequence: seq))
+        XCTAssertTrue(ValidateSubsequence.solution2(input: input, sequence: seq))
     }
     
     func test2() {
         let input = [1]
-        XCTAssertTrue(ValidateSubsequence.solution(input: input, sequence: [1]))
-        XCTAssertTrue(ValidateSubsequence.solution2(input: input, sequence: [1]))
+        let seq = [1]
+        
+        XCTAssertTrue(ValidateSubsequence.solution(input: input, sequence: seq))
+        XCTAssertTrue(ValidateSubsequence.solution2(input: input, sequence: seq))
     }
     
     func test3() {
         let input = [1, 2, 3]
-        XCTAssertFalse(ValidateSubsequence.solution(input: input, sequence: [3, 4]))
-        XCTAssertFalse(ValidateSubsequence.solution2(input: input, sequence: [3, 4]))
+        let seq = [3, 4]
+        
+        XCTAssertFalse(ValidateSubsequence.solution(input: input, sequence: seq))
+        XCTAssertFalse(ValidateSubsequence.solution2(input: input, sequence: seq))
     }
 }
 
