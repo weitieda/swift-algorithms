3fa66b279759fab9faa8b02f9becafa3dd40a4de
Tieda Wei <weitieda@gmail.com>
2021-01-30 15:30:53 -0500

    Add MonotonicArrayTest

---
+++ Algorithms/AlgorithmsTests/MonotonicArrayTest.swift
@@ -0,0 +1,24 @@
+//
+//  MonotonicArrayTest.swift
+//  AlgorithmsTests
+//
+//  Created by Tieda Wei on 2021-01-30.
+//
+
+import XCTest
+@testable import Algorithms
+
+class MonotonicArrayTest: XCTestCase {
+
+    func test1() {
+
+    }
+
+    func test2() {
+
+    }
+
+    func test3() {
+
+    }
+}
