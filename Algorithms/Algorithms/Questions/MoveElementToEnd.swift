aad7d0365f609bc27d85f5065a698dc30eb0cf98
Tieda Wei <weitieda@gmail.com>
2021-01-30 14:49:25 -0500

    Refactor

--- Algorithms/Algorithms/Questions/MoveElementToEnd.swift
+++ Algorithms/Algorithms/Questions/MoveElementToEnd.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 /*
+ 
  You're given an array of integers and an integer.
+ 
  Write a function that moves all instances of that integer in the array to the end of the array and returns the array.
  The function should perform this in place (i.e., it should mutate the input array)
  and doesn't need to maintain the order of the other integers.
