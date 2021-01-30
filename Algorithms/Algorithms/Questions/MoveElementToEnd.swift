00b848c47e057236547bab40d202c51c26502ce5
Tieda Wei <weitieda@gmail.com>
2021-01-30 11:46:01 -0500

    Make MoveElementToEnd performs in place

--- Algorithms/Algorithms/Questions/MoveElementToEnd.swift
+++ Algorithms/Algorithms/Questions/MoveElementToEnd.swift
@@ -8,36 +8,36 @@
 import Foundation
 
 /*
- You're given an array of integers and an integer. Write a function that moves all instances of that integer in the array to the end of the array and returns the array.
+ You're given an array of integers and an integer.
+ Write a function that moves all instances of that integer in the array to the end of the array and returns the array.
  The function should perform this in place (i.e., it should mutate the input array) and doesn't need to maintain the order of the other integers.
  
  e.g.
  array = [2, 1, 2, 2, 2, 3, 4, 2]
  toMove = 2
- -> [1, 3, 4, 2, 2, 2, 2, 2]  // first 3 nums order doesn't matter
+ -> [1, 3, 4, 2, 2, 2, 2, 2]  // first 3 numbers' order doesn't matter
  
  */
 
 final class MoveElementToEnd {
     
     // t: O(n), s: O(1)
-    static func solution(_ array: [Int], _ toMove: Int) -> [Int] {
-        var result = array
-        
+    static func solution(_ array: inout [Int], _ toMove: Int) -> [Int] {
         var head = 0
-        var tail = result.count - 1
+        var tail = array.count - 1
         
         while head < tail {
-            if result[head] == toMove {
-                while head < tail && result[tail] == toMove {
+            if array[head] == toMove {
+                while head < tail && array[tail] == toMove {
                     tail -= 1
                 }
-                (result[head], result[tail]) = (result[tail], result[head])
+                
+                (array[head], array[tail]) = (array[tail], array[head])
             }
             head += 1
         }
         
-        return result
+        return array
     }
     
 }
--- Algorithms/AlgorithmsTests/MoveElementToEndTest.swift
+++ Algorithms/AlgorithmsTests/MoveElementToEndTest.swift
@@ -11,29 +11,29 @@ import XCTest
 class MoveElementToEndTest: XCTestCase {
     
     func test1() {
-        let arr = [2, 1, 2, 2, 2, 3, 4, 2]
+        var arr = [2, 1, 2, 2, 2, 3, 4, 2]
         
         let exp = [4, 1, 3, 2, 2, 2, 2, 2]
         
-        let sut = MoveElementToEnd.solution(arr, 2)
+        let sut = MoveElementToEnd.solution(&arr, 2)
         XCTAssertEqual(exp, sut)
     }
     
     func test2() {
-        let arr = [1, 2, 2, 2, 3, 4, 2]
+        var arr = [1, 2, 2, 2, 3, 4, 2]
         
         let exp = [1, 2, 2, 2, 3, 2, 4]
         
-        let sut = MoveElementToEnd.solution(arr, 4)
+        let sut = MoveElementToEnd.solution(&arr, 4)
         XCTAssertEqual(exp, sut)
     }
     
     func test3() {
-        let arr = [3, 3, 3, 2, 2, 2]
+        var arr = [3, 3, 3, 2, 2, 2]
         
         let exp = [2, 2, 2, 3, 3, 3]
         
-        let sut = MoveElementToEnd.solution(arr, 3)
+        let sut = MoveElementToEnd.solution(&arr, 3)
         XCTAssertEqual(exp, sut)
     }
 }
