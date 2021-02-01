a9eed3b3f89d0eac502bffe0bc60e7b2fbd1d7b0
Tieda Wei <weitieda@gmail.com>
2021-01-31 23:47:04 -0500

    Add SpiralMatrix

---
+++ Algorithms/Algorithms/Questions/SpiralMatrix.swift
@@ -0,0 +1,72 @@
+//
+//  SpiralMatrix.swift
+//  Algorithms
+//
+//  Created by Tieda Wei on 2021-01-31.
+//
+
+import Foundation
+
+/*
+ 
+ Given an m x n matrix, return all elements of the matrix in spiral order.
+
+
+ Input: matrix =
+ [[1,2,3],
+ [4,5,6],
+ [7,8,9]]
+
+ Output: [1,2,3,6,9,8,7,4,5]
+
+ */
+
+final class SpiralMatrix {
+
+    // t: O(n), s: O(n)
+    static func solution(_ input: [[Int]]) -> [Int] {
+
+        var result: [Int] = []
+
+        var top = 0
+        var right = input[0].count - 1
+        var bottom = input.count - 1
+        var left = 0
+
+        while left < right && top < bottom {
+            for i in stride(from: left, to: right, by: 1) {
+                result.append(input[top][i])
+            }
+
+            for i in stride(from: top, to: bottom, by: 1) {
+                result.append(input[i][right])
+            }
+
+            for i in stride(from: right, to: left, by: -1) {
+                result.append(input[bottom][i])
+            }
+
+            for i in stride(from: bottom, to: top, by: -1) {
+                result.append(input[i][left])
+            }
+
+            top += 1
+            right -= 1
+            bottom -= 1
+            left += 1
+        }
+
+        if left == right {
+            for i in stride(from: top, through: bottom, by: 1) {
+                result.append(input[i][left])
+            }
+        } else if top == bottom {
+            for i in stride(from: left, through: right, by: 1) {
+                result.append(input[top][i])
+            }
+        }
+
+        return result
+    }
+
+}
---
+++ Algorithms/AlgorithmsTests/SpiralMatrixTest.swift
@@ -0,0 +1,51 @@
+//
+//  SpiralMatrixTest.swift
+//  AlgorithmsTests
+//
+//  Created by Tieda Wei on 2021-01-31.
+//
+
+import XCTest
+@testable import Algorithms
+
+class SpiralMatrixTest: XCTestCase {
+
+    func test1() {
+        let matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+
+        expect([1, 2, 3, 6, 9, 8, 7, 4, 5],
+               when: matrix)
+    }
+
+    func test2() {
+        let matrix = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12]
+        ]
+
+        expect([1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7],
+               when: matrix)
+    }
+
+    func test3() {
+        let matrix = [
+            [27, 12, 35],
+            [25, 21, 94],
+            [19, 96, 43],
+            [55, 36, 10]
+        ]
+
+        expect([27, 12, 35, 94, 43, 10, 36, 55, 19, 25, 21, 96],
+               when: matrix)
+    }
+
+    private func expect(_ result: [Int], when input: [[Int]]) {
+        let sut = SpiralMatrix.solution(input)
+        XCTAssertEqual(sut, result)
+    }
+}
