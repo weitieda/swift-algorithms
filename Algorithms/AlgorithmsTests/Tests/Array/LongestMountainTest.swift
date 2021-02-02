10bf39245875fa4994c08e29226bf44cb0e72012
Tieda Wei <weitieda@gmail.com>
2021-02-02 00:01:26 -0500

    Add LongestMountain question

---
+++ Algorithms/AlgorithmsTests/Tests/Array/LongestMountainTest.swift
@@ -0,0 +1,16 @@
+//
+//  LongestMountainTest.swift
+//  AlgorithmsTests
+//
+//  Created by Tieda Wei on 2021-02-02.
+//
+
+import XCTest
+@testable import Algorithms
+
+class LongestMountainTest: XCTestCase {
+
+    func test1() {
+
+    }
+}
