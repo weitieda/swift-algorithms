27751cd72d993b1d8dcf617e350aad1ffce922a9
Tieda Wei <weitieda@gmail.com>
2021-02-05 23:17:43 -0500

    Add FindDuplicateNumber question

---
+++ Algorithms/Algorithms/Questions/Array/FindDuplicateNumber.swift
@@ -0,0 +1,44 @@
+//
+//  FindDuplicateNumber.swift
+//  Algorithms
+//
+//  Created by Tieda Wei on 2021-02-05.
+//
+
+import Foundation
+
+/*
+
+ Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+ There is only one repeated number in nums, return this repeated number.
+ 
+ Example 1:
+ Input: nums = [1,3,4,2,2]
+ Output: 2
+
+ Example 2:
+ Input: nums = [3,1,3,4,2]
+ Output: 3
+
+ Example 3:
+ Input: nums = [1,1]
+ Output: 1
+
+ Example 4:
+ Input: nums = [1,1,2]
+ Output: 1
+ 
+ */
+
+final class FindDuplicateNumber {
+
+    // t: O(), s: O()
+    static func solution(array: [Int]) -> Int {
+
+
+
+        return 0
+    }
+
+}
---
+++ Algorithms/AlgorithmsTests/Tests/Array/FindDuplicateNumberTest.swift
@@ -0,0 +1,24 @@
+//
+//  FindDuplicateNumberTest.swift
+//  AlgorithmsTests
+//
+//  Created by Tieda Wei on 2021-02-05.
+//
+
+import XCTest
+@testable import Algorithms
+
+class FindDuplicateNumberTest: XCTestCase {
+
+    func test1() {
+
+    }
+
+    func test2() {
+
+    }
+
+    func test3() {
+
+    }
+}
