07af85cc4aaeae6cd6b3f02c432afbd458d6d96a
Tieda Wei <weitieda@gmail.com>
2021-02-14 13:25:13 -0500

    Add fib memoization solution

--- Algorithms/Algorithms/Questions/Recursion/NthFibonacci.swift
+++ Algorithms/Algorithms/Questions/Recursion/NthFibonacci.swift
@@ -15,18 +15,28 @@ import Foundation
 
 final class NthFibonacci {
 
-    // t: O(2^n), s: O(1)
+    // t: O(2^n), s: O(n)
     static func solutionTime2n(n: Int) -> Int {
         if n == 1 { return 0 }
         if n == 2 { return 1 }
         return solutionTime2n(n: n - 1) + solutionTime2n(n: n - 2)
     }
 
-    // t: O(n), s: O()
-    static func solutionOnOn(n: Int) -> Int {
-        if n == 1 { return 0 }
-        if n == 2 { return 1 }
-        return solutionTime2n(n: n - 1) + solutionTime2n(n: n - 2)
+
+    // t: O(n), s: O(n)
+    static func solutionMemoization(n: Int) -> Int {
+        var memoization = [1: 0, 2: 1]
+        return memoizationFib(n: n, memoization: &memoization)
     }
 
+    private static func memoizationFib(n: Int, memoization: inout [Int: Int]) -> Int {
+
+        if let value = memoization[n] {
+            return value
+        }
+
+        let newValue = memoizationFib(n: n-1, memoization: &memoization) + memoizationFib(n: n-2, memoization: &memoization)
+        memoization[n] = newValue
+        return newValue
+    }
 }
--- Algorithms/AlgorithmsTests/Tests/Recursion/NthFibonacciTest.swift
+++ Algorithms/AlgorithmsTests/Tests/Recursion/NthFibonacciTest.swift
@@ -16,13 +16,16 @@ class NthFibonacciTest: XCTestCase {
         expect(n: 4, toBe: 2)
         expect(n: 5, toBe: 3)
         expect(n: 6, toBe: 5)
+
+        XCTAssertEqual(39088169,
+                       NthFibonacci.solutionMemoization(n: 39))
     }
 
     func expect(n: Int, toBe result: Int) {
         let sut1 = NthFibonacci.solutionTime2n(n: n)
         XCTAssertEqual(result, sut1)
 
-        let sut2 = NthFibonacci.solutionOnOn(n: n)
+        let sut2 = NthFibonacci.solutionMemoization(n: n)
         XCTAssertEqual(result, sut2)
     }
 }
