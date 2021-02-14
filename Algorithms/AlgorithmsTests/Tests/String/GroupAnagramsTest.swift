b5e1125467179609bbe403ca54725c7c0cb45fd0
Tieda Wei <weitieda@gmail.com>
2021-02-13 22:19:19 -0500

    refactor

--- Algorithms/AlgorithmsTests/Tests/String/GroupAnagramsTest.swift
+++ Algorithms/AlgorithmsTests/Tests/String/GroupAnagramsTest.swift
@@ -12,19 +12,19 @@ class GroupAnagramsTest: XCTestCase {
 
     func test1() {
         expect(result: [["ate", "eat", "tea"], ["bat"], ["nat", "tan"]],
-               input: ["eat", "tea", "tan", "ate", "nat", "bat"])
+               when: ["eat", "tea", "tan", "ate", "nat", "bat"])
 
         expect(result: [["a"]],
-               input: ["a"])
+               when: ["a"])
 
         expect(result: [[""]],
-               input: [""])
+               when: [""])
 
         expect(result: [["123", "132"]],
-               input: ["123", "132"])
+               when: ["123", "132"])
     }
 
-    private func expect(result: [[String]], input: [String]) {
+    private func expect(result: [[String]], when input: [String]) {
         let sut = GroupAnagrams.solution(strs: input)
         XCTAssertEqual(result, sut.map {$0.sorted()}.sorted(by: { $0[0] < $1[0] }))
     }
