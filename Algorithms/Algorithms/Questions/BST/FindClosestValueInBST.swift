b5dd4063564ef4b2f447f2d4fc609284afe464d9
Tieda Wei <weitieda@gmail.com>
2021-02-15 23:09:54 -0500

    Add FindClosestValueInBST

---
+++ Algorithms/Algorithms/Questions/BST/FindClosestValueInBST.swift
@@ -0,0 +1,49 @@
+//
+//  FindClosestValueInBST.swift
+//  Algorithms
+//
+//  Created by Tieda Wei on 2021-02-15.
+//
+
+import Foundation
+
+/*
+ 
+ Write a function that takes in a Binary Search Tree (BST)
+ and a target integer value and returns the closest value
+ to that target value contained in the BST.
+ 
+ You can assume that there will only be one closest value.
+
+ Each BST node has an integer `value`, a `left` child node,
+ and a `right` child node.
+ A node is aid to be valid `BST` node if and only if it
+ satisfies the BST property: it's `value` is strictly greater
+ than the values of every node to its left;
+ its `value` is less than or equal to the values of every node
+ to its right; and it's children modes are either valid `BST`
+ nodes themselves or `Null`.
+
+ e.g.
+
+ tree =     10
+           /   \
+          5    15
+         / \   / \
+        2   5 13 22
+       /       \
+      1         14
+
+ target = 12
+ -> 13
+
+ */
+
+final class FindClosestValueInBST {
+
+    // t: O(), s: O()
+    static func solution() {
+
+    }
+
+}
