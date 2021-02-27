f9c142049bb8e0e994018b1519378fa932d4f2ea
Tieda Wei <weitieda@gmail.com>
2021-02-27 01:46:37 -0500

    Add Combinations

---
+++ Algorithms/Algorithms/Questions/Recursion/Combinations.swift
@@ -0,0 +1,67 @@
+//
+//  Combinations.swift
+//  Algorithms
+//
+//  Created by Tieda Wei on 2021-02-27.
+//
+
+import Foundation
+
+/*
+ 
+ [a, b, c]
+
+ ->
+
+ [
+    [],
+    [a],
+    [b],
+    [c],
+    [a, b],
+    [b, c],
+    [a, c],
+    [a, b, c]
+ ]
+ 
+ */
+
+///
+///                         []
+///     a         []                  [a]                // left node: don't add a; right node: add a
+///     b     []       [b]        [a]       [a,b]        // left node: don't add b; right node: add b
+///     c   []  [c]  [b][b,c]   [a][a,c]  [a,b] [a, b, c]
+///
+/// Every time add a new letter, do two things:
+/// 1. Keep whatever already have
+/// 2. Add new letter to each element of current
+///
+/// call stack:
+/// solution([a,b,c])    -> [[], [c], [b], [c,b], [a], [a,c], [a,b], [a,b,c]]
+/// solution([b,c])      -> [[], [c], [b], [c,b]] // keep [[],[c]] then add b to [] and [c]
+/// solution([c])        -> [[], [c]]  // keep []; add [c]
+/// solution([])         -> [[]]
+///
+final class Combinations {
+
+    // t: O(n^2), s: O(2^n)
+    static func solution(array: [String]) -> [[String]] {
+        if array.isEmpty { return [[]] }
+
+        let firstLetter = array[0]
+        let restLetters = Array(array[1...])
+
+        // left node
+        let combsWithoutFirstLetter = solution(array: restLetters)
+
+        // right node: add first letter in each left node
+        let combsWithFirst = combsWithoutFirstLetter.map { comb -> [String] in
+            var c = comb
+            c.append(firstLetter)
+            return c
+        }
+
+        return combsWithoutFirstLetter + combsWithFirst
+    }
+
+}
---
+++ Algorithms/AlgorithmsTests/Tests/Questions/Recursion/CombinationsTest.swift
@@ -0,0 +1,32 @@
+//
+//  CombinationsTest.swift
+//  AlgorithmsTests
+//
+//  Created by Tieda Wei on 2021-02-27.
+//
+
+import XCTest
+@testable import Algorithms
+
+class CombinationsTest: XCTestCase {
+
+    func test1() {
+        let sut = Combinations.solution(array: ["a", "b", "c"])
+        XCTAssertEqual(sut, [[], ["c"], ["b"], ["c", "b"], ["a"], ["c", "a"], ["b", "a"], ["c", "b", "a"]])
+    }
+
+    func test2() {
+        let sut = Combinations.solution(array: ["a", "b"])
+        XCTAssertEqual(sut, [[], ["b"], ["a"], ["b", "a"]])
+    }
+
+    func test3() {
+        let sut = Combinations.solution(array: ["a"])
+        XCTAssertEqual(sut, [[], ["a"]])
+    }
+
+    func test4() {
+        let sut = Combinations.solution(array: [])
+        XCTAssertEqual(sut, [[]])
+    }
+}
