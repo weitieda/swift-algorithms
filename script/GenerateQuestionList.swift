5fc95943e8eef45d662119d3167d9fd7ac73855c
Tieda Wei <weitieda@gmail.com>
2021-03-07 16:46:43 -0500

    Fix indentation in GenerateQuestionList.swift

--- script/GenerateQuestionList.swift
+++ script/GenerateQuestionList.swift
@@ -8,21 +8,21 @@ let dirEnum = fm.enumerator(atPath: questionPath)
 var questionsDict = [String: [String]]()
 
 while let file = dirEnum?.nextObject() as? String {
-	if file.hasSuffix(".swift") && !file.hasPrefix("Helper") {
-		var comp = file.components(separatedBy: "/")
-		let category = comp.removeFirst()
-		let fileName = comp.removeFirst()
-		questionsDict[category, default: []].append(fileName)
-	}
+    if file.hasSuffix(".swift") && !file.hasPrefix("Helper") {
+        var comp = file.components(separatedBy: "/")
+        let category = comp.removeFirst()
+        let fileName = comp.removeFirst()
+        questionsDict[category, default: []].append(fileName)
+    }
 }
 
 questionsDict.keys.sorted().forEach {
-	print("### **\($0)**")
-	for file in questionsDict[$0]! {
-		let name = file.components(separatedBy: ".")[0]
-		print("[\(name)](Algorithms/Algorithms/Questions/\($0)/\(file))\n")
-		
-	}
-	print()
+    print("### **\($0)**")
+    for file in questionsDict[$0]! {
+        let name = file.components(separatedBy: ".")[0]
+        print("[\(name)](Algorithms/Algorithms/Questions/\($0)/\(file))\n")
+        
+    }
+    print()
 }
 
