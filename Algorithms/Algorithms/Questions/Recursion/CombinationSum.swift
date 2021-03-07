c8ea48765a921534fff652e166723392f5d83913
Tieda Wei <weitieda@gmail.com>
2021-03-07 12:45:17 -0500

    refactor

--- Algorithms/Algorithms/Questions/Recursion/CombinationSum.swift
+++ Algorithms/Algorithms/Questions/Recursion/CombinationSum.swift
@@ -21,7 +21,8 @@ import Foundation
  Input: candidates = [2,3,6,7], target = 7
  Output: [[2,2,3],[7]]
  Explanation:
- 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+ 2 and 3 are candidates, and 2 + 2 + 3 = 7.
+ Note that 2 can be used multiple times.
  7 is a candidate, and 7 = 7.
  These are the only two combinations.
 
