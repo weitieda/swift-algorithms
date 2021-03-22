4072418d85a1c8bd16c4cf445a76f11eff5be8fc
Tieda Wei <weitieda@gmail.com>
2021-03-21 21:37:01 -0400

    Add CourseSchedule

---
+++ Algorithms/Algorithms/Questions/Graph/CourseSchedule.swift
@@ -0,0 +1,45 @@
+//
+//  CourseSchedule.swift
+//  Algorithms
+//
+//  Created by Tieda Wei on 2021-03-21.
+//
+
+import Foundation
+
+/*
+ https://leetcode.com/problems/course-schedule-ii/
+ */
+
+final class CourseSchedule {
+
+    // t: O(), s: O()
+    static func solution(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
+
+        // index->course, value->prerequistesCount
+        var graph = Array(repeating: 0, count: numCourses)
+        for pre in prerequisites {
+            graph[pre[0]] += 1
+        }
+        
+        var readyToTake = [Int]()
+        for i in 0..<numCourses where graph[i] == 0 {
+            readyToTake.append(i)
+        }
+        
+        var path = [Int]()
+        while !readyToTake.isEmpty {
+            let course = readyToTake.removeLast()
+            path.append(course)
+            for pair in prerequisites where pair[1] == course {
+                graph[pair[0]] -= 1
+                if graph[pair[0]] == 0 {
+                    readyToTake.append(pair[0])
+                }
+            }
+        }
+        
+        return path.count == numCourses ? path : []
+    }
+
+}
---
+++ Algorithms/AlgorithmsTests/Tests/Questions/Graph/CourseScheduleTest.swift
@@ -0,0 +1,26 @@
+//
+//  CourseScheduleTest.swift
+//  AlgorithmsTests
+//
+//  Created by Tieda Wei on 2021-03-21.
+//
+
+import XCTest
+@testable import Algorithms
+
+class CourseScheduleTest: XCTestCase {
+
+    func test1() { // TODO
+//        let n = 4
+//        let pre = [[1, 0], [2, 0], [3, 1], [3, 2]]
+//        let sut = CourseSchedule.solution(4, pre)
+    }
+
+    func test2() {
+
+    }
+
+    func test3() {
+
+    }
+}
