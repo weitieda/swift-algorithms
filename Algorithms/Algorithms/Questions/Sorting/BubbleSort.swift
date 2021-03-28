40805bfe9fc0f0f5792bc6a64e4cbd2c0fa36394
Tieda Wei <weitieda@gmail.com>
2021-03-28 00:05:57 -0400

    Array(0...100)

--- Algorithms/Algorithms/Questions/Sorting/BubbleSort.swift
+++ Algorithms/Algorithms/Questions/Sorting/BubbleSort.swift
@@ -14,7 +14,8 @@ import Foundation
 final class BubbleSort {
 
     // t: O(n^2), s: O(1)
-    static func solution(array: inout [Int]) -> [Int] {
+    static func solution(array: [Int]) -> [Int] {
+        var array = array
         for index in array.indices {
             let endIndex = array.endIndex - index // Each round will move largest to the end
             for j in 1..<endIndex where array[j-1] > array[j] {
