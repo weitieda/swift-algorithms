5e081d00b99b967de0c572de1c7decd4583e9585
Tieda Wei <weitieda@gmail.com>
2021-03-27 22:48:03 -0400

    Refactor ReconstructItineraryTest

--- Algorithms/AlgorithmsTests/Tests/Questions/Graph/ReconstructItineraryTest.swift
+++ Algorithms/AlgorithmsTests/Tests/Questions/Graph/ReconstructItineraryTest.swift
@@ -11,33 +11,34 @@ import XCTest
 class ReconstructItineraryTest: XCTestCase {
 
     func test() {
-        expect(["JFK", "MUC", "LHR", "SFO", "SJC"],
-               when: [["MUC", "LHR"],
-                      ["JFK", "MUC"],
-                      ["SFO", "SJC"],
-                      ["LHR", "SFO"]]
-        )
-
-        expect(["JFK", "ATL", "JFK", "SFO", "ATL", "SFO"],
-               when: [["JFK", "SFO"],
-                      ["JFK", "ATL"],
-                      ["SFO", "ATL"],
-                      ["ATL", "JFK"],
-                      ["ATL", "SFO"]]
-        )
-
-        expect(["JFK", "NRT", "JFK", "KUL"],
-               when: [["JFK", "KUL"],
-                      ["JFK", "NRT"],
-                      ["NRT", "JFK"]]
-        )
-    }
-
-    private func expect(_ result: [String], when tickets: [[String]]) {
-        let sut = ReconstructItinerary.solutionDFS(tickets)
-        XCTAssertEqual(sut, result)
-
-        let sut2 = ReconstructItinerary.solutionIterative(tickets)
-        XCTAssertEqual(sut2, result)
+        
+        [
+            (["JFK", "MUC", "LHR", "SFO", "SJC"],
+             [
+                ["MUC", "LHR"],
+                ["JFK", "MUC"],
+                ["SFO", "SJC"],
+                ["LHR", "SFO"]
+             ]),
+            
+            (["JFK", "ATL", "JFK", "SFO", "ATL", "SFO"],
+             [
+                ["JFK", "SFO"],
+                ["JFK", "ATL"],
+                ["SFO", "ATL"],
+                ["ATL", "JFK"],
+                ["ATL", "SFO"]
+             ]),
+            
+            (["JFK", "NRT", "JFK", "KUL"],
+             [
+                ["JFK", "KUL"],
+                ["JFK", "NRT"],
+                ["NRT", "JFK"]
+             ])
+        ].forEach { (result, tickets) in
+            expect(result, when: ReconstructItinerary.solutionDFS(tickets))
+            expect(result, when: ReconstructItinerary.solutionIterative(tickets))
+        }
     }
 }
