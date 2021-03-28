31f898de54db7742c793cd0d6aa0cee19a63e041
Tieda Wei <weitieda@gmail.com>
2021-03-28 01:05:23 -0400

    refactor

--- Algorithms/AlgorithmsTests/Tests/Questions/Array/ThreeSumDistinctTest.swift
+++ Algorithms/AlgorithmsTests/Tests/Questions/Array/ThreeSumDistinctTest.swift
@@ -10,55 +10,32 @@ import XCTest
 
 class ThreeSumDistinctTest: XCTestCase {
 
-    func test1() {
-        let input = [12, 3, 1, 2, -6, 5, -8, 6]
-        let targetSum = 0
-
-        let expected = [[-8, 2, 6], [-8, 5, 3], [-6, 1, 5]]
-
-        let sut = ThreeSumDistinct.solution(input: input, targetSum: targetSum)
-        XCTAssertEqual(expected.count, sut.count)
-        avoidOrder(sut).forEach {
-            XCTAssertTrue(avoidOrder(expected).contains($0))
+    func test() {
+        [
+            (
+                [12, 3, 1, 2, -6, 5, -8, 6],
+                0,
+                [[-8, 2, 6],
+                 [-8, 5, 3],
+                 [-6, 1, 5]]
+            ),
+            (
+                [1, 2, 3, 4, 5, 6, 7, 8, 9, 15],
+                18,
+                [[1, 2, 15],
+                 [1, 8, 9],
+                 [2, 7, 9],
+                 [3, 6, 9],
+                 [3, 7, 8],
+                 [4, 5, 9],
+                 [4, 6, 8],
+                 [5, 6, 7]]
+            )
+        ].forEach { (arr, targetSum, expectedResult) in
+            let s1 = ThreeSumDistinct.solution(input: arr, targetSum: targetSum)
+            let s2 = ThreeSumDistinct.solution2(input: arr, targetSum: targetSum)
+            expect(true, when: isSame2DArray(expectedResult, s1))
+            expect(true, when: isSame2DArray(expectedResult, s2))
         }
-
-        let sut2 = ThreeSumDistinct.solution2(input: input, targetSum: targetSum)
-        XCTAssertEqual(expected.count, sut2.count)
-        avoidOrder(sut2).forEach {
-            XCTAssertTrue(avoidOrder(expected).contains($0))
-        }
-    }
-
-    func test2() {
-        let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 15]
-        let targetSum = 18
-
-        let expected = [
-            [1, 2, 15],
-            [1, 8, 9],
-            [2, 7, 9],
-            [3, 6, 9],
-            [3, 7, 8],
-            [4, 5, 9],
-            [4, 6, 8],
-            [5, 6, 7]
-          ]
-
-        let sut = ThreeSumDistinct.solution(input: input, targetSum: targetSum)
-        XCTAssertEqual(expected.count, sut.count)
-        avoidOrder(sut).forEach {
-            XCTAssertTrue(avoidOrder(expected).contains($0))
-        }
-
-        let sut2 = ThreeSumDistinct.solution2(input: input, targetSum: targetSum)
-        XCTAssertEqual(expected.count, sut2.count)
-        avoidOrder(sut2).forEach {
-            XCTAssertTrue(avoidOrder(expected).contains($0))
-        }
-
-    }
-
-    private func avoidOrder(_ array: [[Int]]) -> [[Int]] {
-        array.map { $0.sorted() }
     }
 }
