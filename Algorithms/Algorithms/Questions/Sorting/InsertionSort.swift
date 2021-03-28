8808551af7501c184e99d6ebb9d6899f0d19bc11
Tieda Wei <weitieda@gmail.com>
2021-03-28 00:11:16 -0400

    Fix InsertionSort crash when count < 2

--- Algorithms/Algorithms/Questions/Sorting/InsertionSort.swift
+++ Algorithms/Algorithms/Questions/Sorting/InsertionSort.swift
@@ -15,15 +15,16 @@ final class InsertionSort {
 
     // t: O(n^2), s: O(1)
     static func solution(array: [Int]) -> [Int] {
-        var array = array
-        for i in 1..<array.endIndex where array[i-1] > array[i] {
+        if array.count < 2 { return array }
+        var arr = array
+        for i in 1..<arr.endIndex where arr[i-1] > arr[i] {
             var j = i
-            while j > 0, array[j] < array[j-1] {
-                array.swapAt(j, j-1)
+            while j > 0, arr[j] < arr[j-1] {
+                arr.swapAt(j, j-1)
                 j -= 1
             }
         }
-        return array
+        return arr
     }
 
 }
