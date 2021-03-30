90b624c71f28e9ec8342f137664bf58a87ee5b0f
Tieda Wei <weitieda@gmail.com>
2021-03-30 01:45:22 -0400

    Add Kadane Algorithms

---
+++ Algorithms/AlgorithmsTests/Tests/Questions/Array/KadaneAlgorithmsMaximumSubarrayTest.swift
@@ -0,0 +1,23 @@
+//
+//  KadaneAlgorithmsMaximumSubarrayTest.swift
+//  AlgorithmsTests
+//
+//  Created by Tieda Wei on 2021-03-30.
+//
+
+import XCTest
+@testable import Algorithms
+
+class KadaneAlgorithmsMaximumSubarrayTest: XCTestCase {
+    func test() {
+        [
+            ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+            ([-2, 100, -3, 4, -1, 2, 1], 103),
+            ([1], 1),
+            ([-1, 2], 2)
+        ].forEach { (arr, result) in
+            expect(result, when: KadaneAlgorithmsMaximumSubarray.solution(nums: arr))
+            expect(result, when: KadaneAlgorithmsMaximumSubarray.solutionKadane(nums: arr))
+        }
+    }
+}
