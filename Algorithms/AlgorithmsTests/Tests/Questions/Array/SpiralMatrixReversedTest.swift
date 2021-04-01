2594d60af9c7ddc06dfa38345864ca011a439425
Tieda Wei <weitieda@gmail.com>
2021-03-31 21:52:31 -0400

    Refactor SpiralMatrixReversedTest

--- Algorithms/AlgorithmsTests/Tests/Questions/Array/SpiralMatrixReversedTest.swift
+++ Algorithms/AlgorithmsTests/Tests/Questions/Array/SpiralMatrixReversedTest.swift
@@ -9,33 +9,25 @@ import XCTest
 @testable import Algorithms
 
 class SpiralMatrixReversedTest: XCTestCase {
-
-    func test1() {
-        let matrix = [[1, 2, 3],
-                      [8, 9, 4],
-                      [7, 6, 5]]
-
-        expect(matrix, n: 3)
-    }
-
-    func test2() {
-        let matrix = [[1, 2],
-                      [4, 3]]
-
-        expect(matrix, n: 2)
-    }
-
-    func test3() {
-        let matrix = [[1, 2, 3, 4],
-                      [12, 13, 14, 5],
-                      [11, 16, 15, 6],
-                      [10, 9, 8, 7]]
-
-        expect(matrix, n: 4)
-    }
-
-    private func expect(_ result: [[Int]], n: Int) {
-        let sut = SpiralMatrixReversed.solution(n)
-        XCTAssertEqual(sut, result)
+    
+    func test() {
+        [
+            ([[1, 2, 3],
+              [8, 9, 4],
+              [7, 6, 5]],
+             3),
+            
+            ([[1, 2],
+              [4, 3]],
+             2),
+            
+            ([[1, 2, 3, 4],
+              [12, 13, 14, 5],
+              [11, 16, 15, 6],
+              [10, 9, 8, 7]],
+             4)
+        ].forEach { (result, n) in
+            expect(result, when: SpiralMatrixReversed.solution(n))
+        }
     }
 }
