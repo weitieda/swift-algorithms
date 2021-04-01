6ede24ab6757e47daf828fc0f92bcaa667f0e7c9
Tieda Wei <weitieda@gmail.com>
2021-03-31 21:46:44 -0400

    Refactor SpiralMatrixTest with expect()

--- Algorithms/AlgorithmsTests/Tests/Questions/Array/SpiralMatrixTest.swift
+++ Algorithms/AlgorithmsTests/Tests/Questions/Array/SpiralMatrixTest.swift
@@ -10,61 +10,37 @@ import XCTest
 
 class SpiralMatrixTest: XCTestCase {
 
-    func test1() {
-        let matrix = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9]
-        ]
-
-        expect([1, 2, 3, 6, 9, 8, 7, 4, 5],
-               when: matrix)
-    }
-
-    func test2() {
-        let matrix = [
-            [1, 2, 3, 4],
-            [5, 6, 7, 8],
-            [9, 10, 11, 12]
-        ]
-
-        expect([1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7],
-               when: matrix)
-    }
-
-    func test3() {
-        let matrix = [
-            [27, 12, 35],
-            [25, 21, 94],
-            [19, 96, 43],
-            [55, 36, 10]
-        ]
-
-        expect([27, 12, 35, 94, 43, 10, 36, 55, 19, 25, 21, 96],
-               when: matrix)
-    }
-
-    func test4() {
-        let matrix = [
-            [1, 2],
-            [3, 4]
-        ]
-
-        expect([1, 2, 4, 3],
-               when: matrix)
-    }
-
-    func test5() {
-        let matrix = [
-            [1]
-        ]
-
-        expect([1],
-               when: matrix)
-    }
-
-    private func expect(_ result: [Int], when input: [[Int]]) {
-        let sut = SpiralMatrix.solution(input)
-        XCTAssertEqual(sut, result)
+    func test() {
+        [
+            ([
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+            ], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
+            
+            ([
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 10, 11, 12]
+            ], [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]),
+            
+            ([
+                [27, 12, 35],
+                [25, 21, 94],
+                [19, 96, 43],
+                [55, 36, 10]
+            ], [27, 12, 35, 94, 43, 10, 36, 55, 19, 25, 21, 96]),
+            
+            ([
+                [1, 2],
+                [3, 4]
+            ], [1, 2, 4, 3]),
+            
+            ([
+                [1]
+            ], [1])
+        ].forEach { (input, result) in
+            expect(result, when: SpiralMatrix.solution(input))
+        }
     }
 }
