598708f1805d677131ab564de238f2e2f14b69f8
Tieda Wei <weitieda@gmail.com>
2021-04-10 23:30:36 -0400

    quick sort

---
+++ Algorithms/Algorithms/Questions/Sorting/QuickSort.swift
@@ -0,0 +1,23 @@
+//
+//  QuickSort.swift
+//  Algorithms
+//
+//  Created by Tieda Wei on 2021-04-10.
+//
+
+import Foundation
+
+/*
+ 
+ */
+
+final class QuickSort {
+
+    // t: O(nlogn), worse case n^2
+    // s: O(1)
+    static func solution<T: Equatable>(_ array: [T]) -> [T] {
+        
+        return array
+    }
+
+}
