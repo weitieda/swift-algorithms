fdf2030eb455eee877e52a1ad131753e9e4b9e83
Tieda Wei <weitieda@gmail.com>
2025-09-16 00:03:49 -0400

    Add impl

--- Algorithms/Algorithms/Questions/Stack/QueueUsingStack.swift
+++ Algorithms/Algorithms/Questions/Stack/QueueUsingStack.swift
@@ -5,22 +5,48 @@
 //  Created by Tieda Wei on 2025-09-15.
 //
 
-import Foundation
-
 public class QueueUsingStack {
-    private var stack1 = [Int]()
+    final class Stack<T> {
+        private var elements = [T]()
+
+        func push(_ element: T) {
+            elements.append(element)
+        }
+
+        @discardableResult
+        func pop() -> T? {
+            elements.popLast()
+        }
+
+        func peek() -> T? {
+            elements.last
+        }
+    }
+
+    private var enqueueStack = Stack<Int>()
+    private var dequeueStack = Stack<Int>()
 
     public init() {}
 
+    // adds x to the end of the queue.
     public func enqueue(_ element: Int) {
-        stack1.append(element)
+        enqueueStack.push(element)
     }
 
+    // removes and returns the element from the front of the queue
     public func dequeue() -> Int? {
-        stack1.removeLast()
+        transformToDequeueStack()
+        return dequeueStack.pop()
     }
 
+    // returns the front element of the queue
     public func peek() -> Int? {
-        stack1.first
+        nil
+    }
+
+    private func transformToDequeueStack() {
+        while let element = enqueueStack.pop() {
+            dequeueStack.push(element)
+        }
     }
 }
--- Algorithms/AlgorithmsTests/Tests/Questions/Stack/QueueUsingStackTest.swift
+++ Algorithms/AlgorithmsTests/Tests/Questions/Stack/QueueUsingStackTest.swift
@@ -12,14 +12,17 @@ class QueueUsingStackTest: XCTestCase {
     func test_enqueue_dequeue() {
         let sut = QueueUsingStack()
         sut.enqueue(1)
+        sut.enqueue(2)
         XCTAssertEqual(sut.dequeue(), 1)
     }
 
     func test_peek() {
-        let sut = QueueUsingStack()
-        sut.enqueue(1)
-        sut.enqueue(2)
-
-        XCTAssertEqual(sut.peek(), 1)
+//        let sut = QueueUsingStack()
+//        sut.enqueue(1)
+//        sut.enqueue(2)
+//
+//        XCTAssertEqual(sut.peek(), 1)
     }
+
+    func test_peek_remainSize() {}
 }
