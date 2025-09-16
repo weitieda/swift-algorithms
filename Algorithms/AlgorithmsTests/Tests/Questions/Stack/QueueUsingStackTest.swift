bfb420eb632ea66c28f3b537999fea311540b32d
Tieda Wei <weitieda@gmail.com>
2025-09-16 13:24:13 -0400

    add test case

--- Algorithms/AlgorithmsTests/Tests/Questions/Stack/QueueUsingStackTest.swift
+++ Algorithms/AlgorithmsTests/Tests/Questions/Stack/QueueUsingStackTest.swift
@@ -58,4 +58,33 @@ class QueueUsingStackTest: XCTestCase {
         XCTAssertEqual(sut.dequeue(), 4)
         XCTAssertEqual(sut.dequeue(), 5)
     }
+
+    func test_mixedOperations() {
+        let sut = QueueUsingStack()
+        sut.enqueue(1)
+        sut.enqueue(2)
+        XCTAssertEqual(sut.peek(), 1)
+        XCTAssertEqual(sut.peek(), 1)
+        sut.enqueue(3)
+        XCTAssertEqual(sut.peek(), 1)
+        sut.enqueue(4)
+
+        XCTAssertEqual(sut.peek(), 1)
+        XCTAssertEqual(sut.dequeue(), 1)
+
+        sut.enqueue(5)
+        XCTAssertEqual(sut.peek(), 2)
+
+        XCTAssertEqual(sut.dequeue(), 2)
+        XCTAssertEqual(sut.dequeue(), 3)
+        XCTAssertEqual(sut.dequeue(), 4)
+        XCTAssertEqual(sut.dequeue(), 5)
+
+        sut.enqueue(6)
+        sut.enqueue(7)
+        sut.enqueue(8)
+        XCTAssertEqual(sut.peek(), 6)
+
+        XCTAssertEqual(sut.dequeue(), 6)
+    }
 }
